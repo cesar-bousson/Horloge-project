dedc8e79a8abb910cafb3ec650833602d3c124ae
cesar-bousson <cesar.bousson@laplateforme.io>
2025-01-13 01:34:23 +0100

    Changes in Horloge_poo.py for optimization in _init_ & more + comments

--- horloge_poo.py
+++ horloge_poo.py
@@ -5,41 +5,47 @@ import pygame
 import threading
 import datetime
 
+
 class PyClock:
     def __init__(self,date:tuple,am_pm):
         self.date = date
         self.am_pm = am_pm
         #AM/PM SETTINGS
-        if self.am_pm == 12 or self.am_pm == 24:
-            pass
-        else:
-            raise TypeError("Are you idiot or what ?")
+        if self.am_pm not in [12, 24]:
+            raise ValueError("Only two choices possible, do better next time...")
         #CHECKING
-        if self.date[0] > 24 or self.date[1] > 60 or self.date[2] > 60:
-            raise TypeError("Please set a normal date.")
-        if self.am_pm == 12:
-            if self.date[0] >= 12:
-                raise TypeError("Please set date to 12h format.")
+        try:
+            hour, minute, second = self.date
+            if not (0<= hour <24 and 0<= minute <60 and 0<= second <60): # optimized # 
+                raise ValueError("Please set a normal date.")
+            elif self.am_pm == 12:
+                if not (0<= hour <12):
+                    raise ValueError("Please set date to 12h format.")
+        except ValueError:
+            print("Error, restart program.")
+            raise 
+        # verified by -> print(f"Horloge configurée : {self.date[0]}h:{self.date[1]}m:{self.date[2]}s, Format AM/PM: {self.am_pm}.")
 
-    def get_date(self): #Force à la complexité ...
+    def get_date(self): 
         """Function displaying time with hh:mm:ss format"""
         if self.date[0] < 10 and self.date[1] < 10 and self.date[2] < 10:#0 0 0
             return f"0{self.date[0]} : 0{self.date[1]} : 0{self.date[2]}"
-        if self.date[0] >= 10 and self.date[1] >= 10 and self.date[2] >= 10:#. . .
+        elif self.date[0] >= 10 and self.date[1] >= 10 and self.date[2] >= 10:#. . .
             return f"{self.date[0]} : {self.date[1]} : {self.date[2]}"
-        if self.date[0] < 10 and self.date[1] >= 10 and self.date[2] < 10:#0 . 0
+        elif self.date[0] < 10 and self.date[1] >= 10 and self.date[2] < 10:#0 . 0
             return f"0{self.date[0]} : {self.date[1]} : 0{self.date[2]}"
-        if self.date[0] >= 10 and self.date[1] < 10 and self.date[2] >= 10:#. 0 .
+        elif self.date[0] >= 10 and self.date[1] < 10 and self.date[2] >= 10:#. 0 .
             return f"{self.date[0]} : 0{self.date[1]} : {self.date[2]}"
-        if self.date[0] < 10 and self.date[1] >= 10 and self.date[2] >= 10:#0 . .
+        elif self.date[0] < 10 and self.date[1] >= 10 and self.date[2] >= 10:#0 . .
             return f"0{self.date[0]} : {self.date[1]} : {self.date[2]}"
-        if self.date[0] >= 10 and self.date[1] >= 10 and self.date[2] < 10:#. . 0
+        elif self.date[0] >= 10 and self.date[1] >= 10 and self.date[2] < 10:#. . 0
             return f"{self.date[0]} : {self.date[1]} : 0{self.date[2]}"
-        if self.date[0] < 10 and self.date[1] < 10 and self.date[2] >= 10:#0 0 .
+        elif self.date[0] < 10 and self.date[1] < 10 and self.date[2] >= 10:#0 0 .
             return f"0{self.date[0]} : 0{self.date[1]} : {self.date[2]}"
-        if self.date[0] >= 10 and self.date[1] < 10 and self.date[2] < 10:#. 0 0
+        elif self.date[0] >= 10 and self.date[1] < 10 and self.date[2] < 10:#. 0 0
             return f"{self.date[0]} : 0{self.date[1]} : 0{self.date[2]}"
-
+        # or only this --->  return ":".join([f"{x:02d}" for x in self.date])
+        
     def set_date(self,times:tuple,form):
         """Methdod changing date/format."""
         self.date = times
@@ -56,7 +62,7 @@ class PyClock:
         """Allow user to pause, resume or stop the time."""
         print(self.get_date(), "\nPress space to pause time\nPress CTRL + C to stop time.")
         if keyboard.is_pressed("space"):
-            print(f"Time paused. Type $ to resume. Type CTRL + C to stop.")
+            print("Time paused. Type $ to resume. Type CTRL + C to stop.")
             while not keyboard.is_pressed("$"):
                 copy = list(self.date)
                 self.date = copy
@@ -64,6 +70,7 @@ class PyClock:
                 if keyboard.is_pressed("$"):
                     self.date = tuple(self.date)
                     print("Time resumed")
+        # add a loop for more reactivity
 
     def live_timing(self):
         """Display live timing"""
@@ -101,6 +108,12 @@ class PyClock:
             time.sleep(1)
 
     def alarm(self,delta:tuple):
+        # try:
+        #     import keyboard
+        #     import pygame
+        # except ImportError:
+        #     print("Veuillez installer les modules `keyboard` et `pygame` pour utiliser toutes les fonctionnalités.")
+        # (optimized)
 
         def play_music(music):
             pygame.mixer.init()
@@ -123,7 +136,7 @@ class PyClock:
 
                 music_thread.start()
                 input_thread.start()
-                return "RRRRIIIIIINNNNNNNGGGGGGGGG"
+                return " WARNING ! ALARM IS ON ! RRRRIIIIIINNNNNNNGGGGGGGGG"
 
     def stopwatch(self):
         """Set a stopwatch."""
@@ -144,19 +157,19 @@ class PyClock:
             input()
             pygame.mixer.stop()
         if len(chrono) != 3:
-            raise TypeError("Veuillez entrer exactement 3 éléments minimum dans la liste (h,m,s)")
+            raise ValueError("Veuillez entrer exactement 3 éléments minimum dans la liste (h,m,s)")
         if chrono[0] >= 24 or chrono[1] >= 60 or chrono[2] >= 60:
-            raise TypeError("Please set a normal date.")
+            raise ValueError("Please set a normal date.")
         while chrono != [0,0,0]:
             os.system("cls" if os.name == "nt" else "clear")
             self.date = chrono
             chrono[2] -= 1
             if chrono[2] < 0:
-                chrono[2] = 60
+                chrono[2] = 59 #optimized
                 chrono[1] -= 1
             if chrono[1] < 0:
                 chrono[0] -= 1
-                chrono[1] = 60
+                chrono[1] = 59
             self.interact()
             time.sleep(1)
             if chrono == [0,0,0]:
@@ -164,7 +177,7 @@ class PyClock:
                 input_thread = threading.Thread(target=wait_for_input)
                 music_thread.start()
                 input_thread.start()
-                return "RRRRIIIIIINNNNNNNGGGGGGGGG"
+                return " WARNING ! TIME OUT ! RRRRIIIIIINNNNNNNGGGGGGGGG"
 
 if __name__ == "__main__":
     timing = PyClock((11,59,55),12)
